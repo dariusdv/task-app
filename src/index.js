f514e6568ed159895907726f7479cfd4a8970db4
dariusdv <ddariusn@gmail.com>
2020-06-27 18:43:02 +0300

    Setup to upload and delete images

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-// 	if (req.method === 'GET') {
-// 		res.send('GET requests are disabled');
-// 	} else {
-// 		next();
-// 	}
-// });
-
-// app.use((req, res, next) => {
-// 	res.status(503).send('Site is currently down. Check back soon!');
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -25,15 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-// const main = async () => {
-// 	// const task = await Task.findById('5ef732b84011283a942f66fa');
-// 	// await task.populate('owner').execPopulate();
-// 	const user = await User.findById('5ef73bafd94bfe3b74774945');
-// 	await user.populate('tasks').execPopulate();
-// };
-
-// main();
